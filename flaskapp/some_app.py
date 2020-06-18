3ca80a6e93e17d7cf965ac70e1963a9d7719f5ab
Corgimama <62329325+Corgimama@users.noreply.github.com>
2020-06-18 12:20:38 +0800

    Add files via upload

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -17,6 +17,7 @@ def data_to():
     some_value = 10
     return render_template('simple.html', some_str=some_str, some_value = some_value, some_pars=some_pars)
     
+
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
@@ -30,6 +31,8 @@ app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfRyKQZAAAAAAH-jfQ1J5xvJJT0INIuIB4qJwZI'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6LfRyKQZAAAAANuc77UiF9zdxEAPY0cbUGGIQgft'
 app.config['RECAPTCHA_OPTIONS'] = {'theme':'white'}
 #work with standarts
+from flask import Flask
+from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
 
 #form for file downloading
@@ -47,6 +50,7 @@ submit = SubmitField('send')
 #function of transaction processing from 127.0.0.1:5000/net
 from werkzeug.utils import secure_filename
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import net as neuronet
 @app.route("/net", methods=['GET','POST'])
 def net():
@@ -65,7 +69,29 @@ def net():
         #file saving
         form.upload.data.save(filename)
     return render_template('net.html', form=form, image_name=filename, neurodic=neurodic)
-    
 
 if __name__ == "__main__":
   app.run(host='127.0.0.1', port=5000)
+  
+from flask import request
+from flask import Response
+import base64
+from PIL import Image
+from io import BytesIO
+import json
+
+@app.route("/apinet", methods=['GET', 'POST'])
+def apinet():
+    if request.mimetype == 'application/json':
+        data = request.get_json()
+        filebytes = data['imagebin'].encode('utf-8')
+        cfile = base64.b64decode(filebytes)
+        img = Image.open(BytesIO(cfile))
+        decode = neuronet.getresult([img])
+        neurodic = {}
+        for elem in decode:
+            neurodic[elem[0][1]] = str(elem[0][2])
+            print(elem)
+    ret = json.dumps(neurodic)
+    resp = Response(response=ret, status=200, mimetype = "application/json")
+    return resp
